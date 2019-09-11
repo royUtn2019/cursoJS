9c1b1b7ad21e13cca7c34e4eb12e575bfc93e2d7
royUtn2019 <54943961+royUtn2019@users.noreply.github.com>
2019-09-11 16:24:40 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
+
+switch(mes){
+
+    case "Enero" : alert("que comiences bien el año!!!."); break;
+    case "Marzo" : alert("a clases!!!."); break;
+    case "Julio" : alert("qse vienen las vacaciones!!!."); break;
+    case "Diciembre" : alert("Felices fiesta!!!."); break;
+    default: alert("Default");
+}
 
 
 
